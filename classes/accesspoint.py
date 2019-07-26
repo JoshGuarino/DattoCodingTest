d53392650ad97dd46071d6486149874c7ec155c7
JoshGuarino <guar0638@fredonia.edu>
2019-07-25 21:55:02 -0400

    able to simulate one accesspoint

--- classes/accesspoint.py
+++ classes/accesspoint.py
@@ -3,24 +3,27 @@ import time
 
 class AccessPoint:
     def __init__(self):
-        self.check_in_time =  round(random.uniform(0, 5), 2)
+        self.check_in_offset =  round(random.uniform(0, 5), 2)
         self.download_time =  round(random.uniform(0, 5), 2)
         self.upgrade_time =  round(random.uniform(0, 5), 2)
         self.download_firmware = False
         self.download_complete = False
         self.upgrade_complete = False
-
-    def check_in_1(self):
-        time.sleep(self.check_in_time)
-        self.download_firmware = True
-        print('download started - ', self.check_in_time, self.download_firmware)
-
-    def check_in_2(self):
-        time.sleep(self.download_time)
-        self.download_complete = True
-        print('download complete, upgrade started - ', self.download_time, self.download_complete)
-
-    def check_in_3(self):
+    
+    def upgrade(self):
         time.sleep(self.upgrade_time)
-        self.upgrade_complete = True
-        print('upgrade complete - ', self.upgrade_time, self.upgrade_complete)
+
+    def download(self):
+        time.sleep(self.download_time)
+
+    def check_in(self):
+        if self.download_firmware==False and self.download_complete==False and self.upgrade_complete==False:
+            time.sleep(self.check_in_offset)
+            self.download_firmware = True
+            print('download started', self.check_in_offset)
+        elif self.download_firmware==True and self.download_complete==False and self.upgrade_complete==False:
+            self.download_complete = True
+            print('download complete', self.download_time)
+        elif self.download_firmware==True and self.download_complete==True and self.upgrade_complete==False:
+            self.upgrade_complete = True
+            print('upgrade complete', self.upgrade_time)
--- sim1.py
+++ sim1.py
@@ -1,15 +1,33 @@
 import time
 from classes import gateway, repeater
+import threading
 
-ap = gateway.Gateway(22)
+
+x = gateway.Gateway(22)
+
+def download(time_amount):
+    time.sleep(time_amount)
+
+def upgrade(time_amount):
+    time.sleep(time_amount)
+
+
+def run(ap):
+    t1 = threading.Thread(target = download, args=[ap.download_time])
+    t2 = threading.Thread(target = upgrade, args=[ap.upgrade_time])
+    while ap.upgrade_complete == False:
+        if t1.is_alive() == False and t2.is_alive() == False:
+            ap.check_in()
+        if ap.download_firmware == True and ap.download_complete == False and ap.upgrade_complete == False and t1.is_alive() == False:
+            t1.start()
+        if ap.download_firmware == True and ap.download_complete == True and ap.upgrade_complete == False and t2.is_alive() == False:
+            t2.start()
+        if ap.upgrade_complete == True:
+            break    
+        time.sleep(5)
 
 
 start = time.time()
-
-ap.check_in_1()
-ap.check_in_2()
-ap.check_in_3()
-
+run(x)
 end = time.time()
-total_time = end - start
-print('total time - ', round(total_time, 2))
+print('total time - ', round(end-start, 2), 'mins')
